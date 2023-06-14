576797921faf76f90223ef5934ec2ab9ff8cfc4d
jun-yunan <junyunan341695@gmail.com>
2023-06-14 14:04:37 +0700

    Connect DB

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -8,7 +8,10 @@ async function connect() {
             useUnifiedTopology: true,
         };
 
-        await mongoose.connect(process.env.MONGODB_URI, options);
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vozbhg5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+            options,
+        );
         console.log('Connected to the database successfully!');
     } catch (error) {
         console.error('Error connecting to the database:', error.message);
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// const productController = require('../app/controllers/ProductController');
 const productController = require('../app/controllers/ProductController');
 const userController = require('../app/controllers/UserController');
 
 const userAuthenticationMiddleware = require('../app/middleware/userAuthenticationMiddleware');
 
 //  USER
-router.get(
-    '/getAllUser',
-    // userAuthenticationMiddleware,
-    userController.getAllUser,
-);
+router.get('/getAllUser', userAuthenticationMiddleware, userController.getAllUser);
 router.post('/signup', userController.signup);
 router.post('/signIn', userController.signIn);
 router.get('/getUser/:id', userController.getUserById);
 
 // PRODUCT
-
 router.get('/products', productController.getProducts);
 
 module.exports = router;
